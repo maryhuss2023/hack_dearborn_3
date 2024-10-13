c683f2dc3e99094253f6240d31990d8e262012bb
Namaa Mokhtar <nmokh05@gmail.com>
2024-10-13 11:00:19 -0400

    wa

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document.getElementById("calculate").addEventListener("click", () => {
-    const amazon_url = "https://www.amazon.com/APHERMA-Massage-Athletes-Handheld-Massager/dp/B09P1DV7D8/ref=sr_1_7?dib=eyJ2IjoiMSJ9.LiyZsN8kTV-tXEtamWyFhFCoKQGkSwqDkNW7Vr4XOjC6cXND_kBqUxFQV-O4KerjA7LLdvMTviqSH9K_Ge0HaL1R_ZI_S768tx5hBPHcoVQ6IfY61CMMjqWoFsgohn76sQG7l1heQEGHwbiRe4WeyqOyavUWA7YqmVPTeHXkdIUHVUM4HwF578tT08yXS9eAukDyepHsOqfaA0xwT5uC2yV3Vh6LARvUEn84NzQxhng3YWBxQcUONQGmN9ihGKggHSrMnlIwj6iAA8nh2tCkUIMY3_zs5KiUl4cPbBXf1a8.k6K1mpXP_9xYS00DlC1j_rzKZIYgPtHL781HWtyyI4w&dib_tag=se&keywords=technology&qid=1728754938&sr=8-7";  // Replace PRODUCT_ID with an actual product ID
+    const amazon_url = "https://www.amazon.com/Tootsie-Pops-Limited-Flavors-Caramel/dp/B004R6EP3Q/ref=pd_ci_mcx_pspc_dp_2_t_1?pd_rd_w=4HOmA&content-id=amzn1.sym.2dfc1605-9ad4-4767-b699-ffe3b61e0dc9&pf_rd_p=2dfc1605-9ad4-4767-b699-ffe3b61e0dc9&pf_rd_r=FZBB5KFQN60N6P452HT0&pd_rd_wg=PpH6D&pd_rd_r=cbfc7668-70af-43cb-a240-f719302227ba&pd_rd_i=B004R6EP3Q";  // Replace PRODUCT_ID with an actual product ID
 
     chrome.runtime.sendMessage({
         action: "calculate_co2",
